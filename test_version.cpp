be6f4c257278f8e8c79648a3e2aaf3788b734795
Dolmatova Varvara <2002varya@gmail.com>
2024-11-10 22:12:11 +0300

    finish version #7

--- test_version.cpp
+++ test_version.cpp
@@ -4,11 +4,11 @@
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUIT(test_version)
+BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    BOOST_CHECK(version() > 0)
+        BOOST_CHECK(version() > 0);
 }
 
-BOOST_AUTO_TEST_SUIT_END()
+BOOST_AUTO_TEST_SUITE_END()
