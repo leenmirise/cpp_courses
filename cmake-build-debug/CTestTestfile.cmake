99f4f52d3553859b504940b352622f37e548bf6d
Dolmatova Varvara <2002varya@gmail.com>
2024-11-10 21:29:16 +0300

    finish version

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/LeenM/CLionProjects/c++_courses
+# Build directory: C:/Users/LeenM/CLionProjects/c++_courses/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test_version]=] "test_version")
+set_tests_properties([=[test_version]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/LeenM/CLionProjects/c++_courses/CMakeLists.txt;73;add_test;C:/Users/LeenM/CLionProjects/c++_courses/CMakeLists.txt;0;")
